ffbda575cd60f56e67a275c224e193fd2bc809d5
JaeHoon Lee <ljhgood9925@gmail.com>
2020-11-20 15:15:38 +0900

    자바에서 값 받기 Request.getPrameter(변수이름);

--- src/com/ServletJSPStudy/www/NaverClass.java
+++ src/com/ServletJSPStudy/www/NaverClass.java
@@ -1,8 +1,7 @@
 package com.ServletJSPStudy.www;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
+
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -17,15 +16,26 @@ public class NaverClass extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-//		OutputStream os = resp.getOutputStream();
-//		PrintStream out = new PrintStream(os, true);
-//		out.println("Hello!<br/>");
-//		out.println("Hello!<br/>");
-//		out.println("Hello!<br/>");
-//		out.println("Hello!<br/>");
+
+		// 앞으로 UTF-8로 출력한다고 설정
+		resp.setCharacterEncoding("UTF-8");
+		// 브라우저 컨텐츠를 UTF-8로 읽어라 설정
+		resp.setContentType("text/html; charset=UTF-8"); 
+		
+		// cnt 인자 값 받기
+		String cnt_ = req.getParameter("cnt");
+
+		int cnt = 100;
+		
+		if (cnt_ != null && !cnt_.equals("")) {
+			cnt = Integer.parseInt(cnt_);
+		}
 		
 		PrintWriter out = resp.getWriter();
-		out.println("Hello");
+
+		for (int i = 0; i < cnt; i++) {
+			out.println((i + 1) + ": 안녕 Hello");
+		}
 	}
 
 }
